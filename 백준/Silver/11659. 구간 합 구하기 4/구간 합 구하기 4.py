cb231435359f48786ff4b9b0d674aa01b08e3246
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2024-11-10 12:19:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 220 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -7,10 +7,6 @@ prefix_sum = [0] * (N + 1)
 for i in range(1, N + 1):
     prefix_sum[i] = prefix_sum[i - 1] + nums[i - 1]
 
-results = []
 for _ in range(M):
-    i, j = map(int, input().split())
-    result = prefix_sum[j] - prefix_sum[i - 1]
-    results.append(result)
-    
-print("\n".join(map(str, results)))
+    i, j = map(int, sys.stdin.readline().split())
+    print(prefix_sum[j] - prefix_sum[i - 1])
