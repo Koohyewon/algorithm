7bfcfbd44db275cae452155d0221625bce3ba608
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2024-11-24 23:36:16 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,8 @@
+import sys
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+c = int(sys.stdin.readline())
+result = list(str(a*b*c))
+
+for i in range(10):
+	print(result.count(str(i)))
