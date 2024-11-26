3f07fd44d2ed0d24236746b71a27a74370d3ea50
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2024-11-26 21:34:36 +0900

    [Silver I] Title: 돌다리, Time: 776 ms, Memory: 42192 KB -BaekjoonHub

---
+++ 백준/Silver/12761. 돌다리/돌다리.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+a,b,n,m=map(int, sys.stdin.readline().split())
+dp = dict()
+dp[n] = 0
+queue = deque([n])
+
+while queue:
+    current = queue.popleft()
+
+    for i in range(10):
+        move = [current+1,current-1,current*a,-current*a,current*b,-current*b,current+a,current-a,current+b,current-b]
+        val = move[i]
+        if 0 <= val <= 100000 and val not in dp:
+            dp[val] = dp[current] + 1
+            queue.append(val)
+
+    if m in dp:
+        break
+    
+print(dp[m])
