da6be512c62da9f9e490ae7967c54c52ef5ec22f
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2024-12-06 13:51:40 +0900

    [Silver I] Title: 쿠키크루, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/22939. 쿠키크루/쿠키크루.py
+++ 백준/Silver/22939. 쿠키크루/쿠키크루.py
@@ -1,6 +1,7 @@
+from itertools import permutations
+
 n = int(input())
 grid = [input().strip() for _ in range(n)]
-
 home = ""
 end = ""
 cookies = {"J": [], "C": [], "B": [], "W": []}
@@ -14,44 +15,32 @@ for i in range(n):
         elif grid[i][j] in "JCBW":
             cookies[grid[i][j]].append((j, i))
 
-# 두 좌표 사이 거리 계산
 def dist(start, end):
-    return abs(start[0] - end[0]) + abs(start[1] - end[1])  # abs 절댓값
+    return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
-def min_distance(topping):
+def min_distance(topping, home, end, cookies):
+    locations = cookies[topping]
+    
+    # 3개 쿠키 위치의 모든 경우
     min_dist = float('inf')
-    visited = [False] * 3 
-
-    def search_path(path, current_dist):
-        nonlocal min_dist
-
-        if len(path) == 3:
-            total_dist = current_dist
-            total_dist += dist(home, cookies[topping][path[0]])  # Home ~ 1 토핑 
-            for i in range(1, 3):
-                total_dist += dist(cookies[topping][path[i - 1]], cookies[topping][path[i]])  # 1 ~ 2 토핑, 2 ~ 3 토핑
-            total_dist += dist(cookies[topping][path[-1]], end)  # 3 토핑 ~ 쿠키크루삥뽕
-            min_dist = min(min_dist, total_dist)
-            return
-
-        for i in range(3):
-            if not visited[i]:
-                visited[i] = True
-                search_path(path + [i], current_dist)
-                visited[i] = False
-
-    search_path([], 0)
+    for perm in permutations(range(len(locations)), 3):
+        # 집 -> 1번 쿠키 -> 2번 쿠키 -> 3번 쿠키 -> 쿠키크루삥뽕
+        current_dist = (
+            dist(home, locations[perm[0]]) +
+            dist(locations[perm[0]], locations[perm[1]]) +
+            dist(locations[perm[1]], locations[perm[2]]) +
+            dist(locations[perm[2]], end)
+        )
+        min_dist = min(min_dist, current_dist)
+    
     return min_dist
 
-# 각 최소 거리
 results = {
-    "Assassin": min_distance("J"),
-    "Healer": min_distance("C"),
-    "Mage": min_distance("B"),
-    "Tanker": min_distance("W")
+    "Assassin": min_distance("J", home, end, cookies),
+    "Healer": min_distance("C", home, end, cookies),
+    "Mage": min_distance("B", home, end, cookies),
+    "Tanker": min_distance("W", home, end, cookies)
 }
 
-# 거리가 가장 작은 값을 찾고, 동일한 거리가 있으면 우선순위 순으로
 sorted_results = sorted(results.items(), key=lambda x: (x[1], x[0]))
-
 print(sorted_results[0][0])
