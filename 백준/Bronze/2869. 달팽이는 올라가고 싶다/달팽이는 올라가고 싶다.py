58539096711f82c595631687852c0e111668b43a
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2024-12-18 16:34:57 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+
+a, b, v = map(int,input().split())
+
+if (v - b) % (a - b) == 0:
+    days = (v - b) // (a - b)
+else:
+    days = (v - b) // (a - b) + 1
+
+print(days)
