4243421d312a7e4e31eb8beab2fd039c5a4ccb76
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2024-12-30 21:17:25 +0900

    Update 색종이 만들기.py

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,23 +1,23 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-n=int(input())
+n = int(input())
 paper = [list(map(int, input().split())) for _ in range(n)]
 
 white_count = 0
 blue_count = 0
 
 def count_paper(paper, x, y, size):
-    start_color=paper[x][y]
+    start_color = paper[x][y]
 
-    for i in range(x, x+size):
-        for j in range(y,y+size):
-            if paper[i][j]!=start_color:
-                half=size//2
-                count_paper(paper,x,y,half)
-                count_paper(paper,x+half,y,half)
-                count_paper(paper,x,y+half,half)
-                count_paper(paper,x+half,y+half,half)
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if paper[i][j] != start_color:
+                half = size // 2
+                count_paper(paper, x, y, half)
+                count_paper(paper, x + half, y, half)
+                count_paper(paper, x, y + half, half)
+                count_paper(paper, x + half, y + half, half)
                 return
 
     if start_color == 0:
