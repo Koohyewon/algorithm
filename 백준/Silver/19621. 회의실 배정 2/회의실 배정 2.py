857af1c09db276d3e0ecd5dfa5d67b3e0875b182
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2025-01-17 12:41:06 +0900

    [Silver II] Title: 회의실 배정 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.py
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.py
@@ -1,22 +1,20 @@
-def max_participants(n, meetings):
-    meetings.sort(key=lambda x: x[1])
-    dp = [0] * n
-    dp[0] = meetings[0][2]
-
-    for i in range(1, n):
-
-        include = meetings[i][2]
-
-        for j in range(i - 1, -1, -1):
-            if meetings[j][1] <= meetings[i][0]:
-                include += dp[j]
-                break
-     
-        dp[i] = max(dp[i - 1], include)
-
-    return dp[-1]
-
 n = int(input())
-meetings = [tuple(map(int, input().split())) for _ in range(n)]
 
-print(max_participants(n, meetings))
+# 회의 정보
+array = []
+
+for _ in range(n):
+  # 시작 시간, 끝나는 시간, 회의 인원
+  array.append(list(map(int, input().split())))
+
+# 회의시간 정렬(K번째 회의는 앞, 뒤 회의랑만 겹치고, 다른 회의랑은 겹치지 않는다는 조건)
+array.sort()
+
+#인원
+dp = [0] * n
+dp[0] = array[0][2] 
+
+for i in range(1, n):
+  dp[i] = max(dp[i - 1], dp[i - 2] + array[i][2])
+
+print(dp[n - 1])
