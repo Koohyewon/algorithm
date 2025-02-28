26bc5d068472336c9ebae86dc3fba05ba0c09a72
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2025-02-28 11:21:59 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 140 ms, Memory: 43164 KB -BaekjoonHub

--- 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.py
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.py
@@ -1,14 +1,21 @@
 import sys
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
+
+n = int(input())
 dic = {}
 
-for i in range(2*n-1):
-    name = sys.stdin.readline().strip()
+for _ in range(n):
+    name = input().rstrip()
     if name in dic:
         dic[name] += 1
     else:
-        dic[name] = 0
+        dic[name] = 1
+        
+for _ in range(n-1):
+    name = input().rstrip()
+    dic[name] -= 1
+    
 for i in dic:
-    if dic[i] % 2 == 0:
+    if dic[i]:
         print(i)
         break
