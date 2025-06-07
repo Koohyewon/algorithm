979dbc5b4eba2efcc502d062e8db27752ad45aa2
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2025-06-07 17:14:32 +0900

    [Silver V] Title: 매우 어려운 문제, Time: 1112 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int, input().split())
+
+if n>=m:
+    print(0)
+else:
+    res=1
+    for i in range(1, n+1):        
+        res=(res*i) % m
+    print(res)
+    
