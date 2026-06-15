f7558dd5551848adbb1384eda8285be73320b8a6
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2026-06-15 18:35:23 +0900

    [level 1] Title: 실패율, Time: 5.65 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,31 +1,3 @@
 def solution(N, stages):
-    stage = [0]*(N + 2)
-
-    for s in stages:
-        stage[s] += 1
-
-    clear = stage[-1]
-
-    for i in range(N, 0, -1):
-        now = stage[i]
-
-        if clear+now != 0:
-            stage[i] = now/(clear+now)
-        else:
-            stage[i] = 0
-
-        clear += now
-
-    fail=[]
-
-    for i in range(1, N + 1):
-        fail.append((-stage[i], i))
-
-    fail.sort()
-
-    answer=[]
-
-    for rate, num in fail:
-        answer.append(num)
-
+    answer = []
     return answer
