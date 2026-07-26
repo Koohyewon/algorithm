8e82ade03c02ba6247dd41e9e2b3c63e906edbd1
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2026-07-26 22:50:30 +0900

    [level 5] Title: 방의 개수, Time: 282.75 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.py
@@ -0,0 +1,29 @@
+def solution(arrows):
+    dx = [0, 1, 1, 1, 0, -1, -1, -1]
+    dy = [1, 1, 0, -1, -1, -1, 0, 1]
+
+    visited_node = set()
+    visited_edge = set()
+
+    x, y = 0, 0
+    visited_node.add((x, y))
+
+    answer = 0
+
+    for arrow in arrows:
+        for _ in range(2):
+            nx = x + dx[arrow]
+            ny = y + dy[arrow]
+
+            if (nx, ny) in visited_node:
+                if ((x, y), (nx, ny)) not in visited_edge:
+                    answer += 1
+
+            visited_node.add((nx, ny))
+
+            visited_edge.add(((x, y), (nx, ny)))
+            visited_edge.add(((nx, ny), (x, y)))
+
+            x, y = nx, ny
+
+    return answer
