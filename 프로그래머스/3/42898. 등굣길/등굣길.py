5baeedba9a31547cbf0dd889a7cff3990978646b
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2026-08-02 01:17:57 +0900

    [level 3] Title: 등굣길, Time: 1.32 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,25 @@
+def solution(m, n, puddles):
+    MOD = 1000000007
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    dp[1][1] = 1
+
+    puddles = set(map(tuple, puddles))
+
+    for y in range(1, n + 1):
+        for x in range(1, m + 1):
+
+            if x == 1 and y == 1:
+                continue
+
+            if (x, y) in puddles:
+                dp[y][x] = 0
+                continue
+
+            dp[y][x] = (
+                dp[y - 1][x] +
+                dp[y][x - 1]
+            ) % MOD
+
+    return dp[n][m]
