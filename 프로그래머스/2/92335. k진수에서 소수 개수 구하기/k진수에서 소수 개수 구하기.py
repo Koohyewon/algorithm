8c2ec93d7e3d88a8fdc62f044e791546ba3ccb38
Koohyewon <127907814+Koohyewon@users.noreply.github.com>
2026-08-31 20:34:00 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 62.91 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,36 +1,30 @@
-def is_prime(num):
-    if num < 2:
-        return False
+def solution(n, k):
+    # k진수로 변환
+    result = ""
+    while n > 0:
+        result = str(n % k) + result
+        n //= k
 
+    # 0을 기준으로 나누기
+    arr = result.split("0")
+    answer = 0
+
+    for x in arr:
+        if x == "":
+            continue
+        if is_prime(int(x)):
+            answer += 1
+    return answer
+
+
+def is_prime(x):
+    if x < 2:
+        return False
     i = 2
-    while i * i <= num:
-        if num % i == 0:
+
+    while i * i <= x:
+        if x % i == 0:
             return False
         i += 1
 
     return True
-
-
-def solution(n, k):
-    # n을 k진수로 변환
-    converted = ""
-
-    while n > 0:
-        converted = str(n % k) + converted
-        n //= k
-
-    # 0을 기준으로 숫자를 나눔
-    numbers = converted.split("0")
-
-    answer = 0
-
-    for num in numbers:
-        # 0 때문에 생긴 빈 문자열은 제외
-        if num == "":
-            continue
-
-        # 10진수로 변환해서 소수인지 확인
-        if is_prime(int(num)):
-            answer += 1
-
-    return answer
